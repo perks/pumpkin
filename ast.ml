c804215855665246ca287866f81463c9a5248cdc
Chris Evans <chris.evans93@gmail.com>
2014-11-09 14:00:06 -0500

    Fixed ast

--- ast.ml
+++ ast.ml
@@ -1,4 +1,3 @@
-type op = Plus | Minus | Times | Divide | Modulo
-          | And | Or | Not | Lt | Lte | Gt | Gte
-          | FPipe | BPipe | RCompose | LCompose | Cons
-
+type operator = Plus | Minus | Times | Divide | Modulo
+                | And | Or | Not | Lt | Lte | Gt | Gte
+                | FPipe | BPipe | RCompose | LCompose | Cons
