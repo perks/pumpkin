6f52078316786d66197dcd6af0171cb74b4c4106
Gabby2212 <gabymelchior22@gmail.com>
2014-11-23 17:27:32 -0500

    Added bool and assign

--- src/ast.mli
+++ src/ast.mli
@@ -1,12 +1,17 @@
-type operator =
-  Plus | Minus | Times | Divide | Modulo
+type operator = 
+  Plus | Minus | Times | Divide | Modulo | Eq | Neq | Gt | Lt | Gte | Lte
+
+type tType = 
+	TNum | TUnit | TBool 
 
 type expression =
     IntLiteral of int
-  | Binop of expression * operator * expression
+  | BoolLiteral of bool
   | Unit
+  | Binop of expression * operator * expression
+  | TypeAssign of string * tType * expression
 
-type 'a body = ('a expression) list
+type 'a body = expression list
 type 'a block = 'a body
 
 type root = expression body
