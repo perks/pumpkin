b096005e755b2935a4f9d277b5550def4ad9a072
Clement Robbins <cjr2151@columbia.com>
2014-11-29 15:57:42 -0500

    fixing makes

--- src/pumpkin.ml
+++ src/pumpkin.ml
@@ -1,6 +1,6 @@
 let _ =
   let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
+  let program = Parser.root Scanner.token lexbuf in
   let sast = Analyzer.annotate_program program in
   Interpret.run sast
 
