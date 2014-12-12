c139cf6ad679b0e1c501af0ee0b65178cbdfd629
Chris Evans <chris.evans93@gmail.com>
2014-12-12 15:15:02 -0500

    If/else block + semicolon cleanups

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,6 +1,25 @@
 open Ast
 open Sast
 
+let rec strip_semicolon l =
+  match l with 
+      [] -> ' '::[]
+    | (hd::tl) -> if (hd = ';') then ' '::(strip_semicolon tl) else hd::(strip_semicolon tl)
+
+let explode s =
+  let rec exp i l =
+    if i < 0 then l 
+    else exp(i - 1)(s.[i] :: l) in
+      exp(String.length s - 1)[]
+
+let implode l =
+  let res = String.create(List.length l) in
+  let rec imp i = function
+      [] -> res
+    | c :: l -> res.[i] <-c; imp(i + 1) l in
+      imp 0 l
+
+
 let flip_last = fun l ->
   let r = List.rev l
   in (List.hd r) :: (List.rev (List.tl r))
@@ -38,8 +57,15 @@ let rec expression_to_string = function
       " = " ^
       expression_to_string e ^ ";"
   | AIfBlock(e, e_list, _) ->
-      "\nif(" ^ expression_to_string e ^ ") {" ^
-      "\t" ^ String.concat "\n\t" (List.map expression_to_string e_list) ^
+      implode(strip_semicolon (explode("\nif(" ^ expression_to_string e ^ ") {"))) ^
+      "\n" ^ String.concat "\n\t" (List.map expression_to_string e_list) ^
+      "\n}\n"
+  | AIfElseBlock(e, e_list1, e_list2, _) -> 
+      implode(strip_semicolon (explode("\nif(" ^ expression_to_string e ^ ") {"))) ^
+      "\n\t" ^ String.concat "\n\t" (List.map expression_to_string e_list1) ^
+      "\n}\n" ^
+      "else {" ^
+      "\n\t" ^ String.concat "\n\t" (List.map expression_to_string e_list2) ^
       "\n}\n"
   | AParameter(id, t) -> id
   | ATypeFuncDecl(id, p_list, e_list, t) ->
@@ -47,11 +73,11 @@ let rec expression_to_string = function
         if (List.length p_list) <> 0 then
           "\nfunction " ^ id ^ "(" ^  
           String.concat ", " (List.map expression_to_string (List.rev p_list)) ^ ")" ^
-          " \n{\n\t" ^ String.concat "\n\t" (List.map expression_to_string e_list) ^
+          " \n{\n" ^ String.concat "\n\t" (List.map expression_to_string e_list) ^
           "\n};\n"
         else 
          "\nfunction " ^ id ^ "() {" ^
-         "\n\t" ^ String.concat "\n\t" (List.map expression_to_string e_list) ^
+         "\n" ^ String.concat "\n\t" (List.map expression_to_string e_list) ^
          "\n};\n"
       else 
         if (List.length p_list) <> 0 then
@@ -70,7 +96,7 @@ let rec expression_to_string = function
   | AFuncCall(id, p_list, _) ->
       if (List.length p_list) <> 0 then
         id ^ "(" ^ 
-        String.concat ", " (List.map expression_to_string (List.rev p_list)) ^ 
+        implode(strip_semicolon (explode(String.concat ", " (List.map expression_to_string (List.rev p_list))))) ^ 
         ");"
       else
         id ^ "();"
